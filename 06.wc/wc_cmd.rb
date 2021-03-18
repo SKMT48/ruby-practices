b95674ef8a6bcdeee1227652e33abd3939aecd51
Sakamoto <tongararin@gmail.com>
2021-03-18 12:54:29 +0900

    一部メソッドを削除、名前を変更

--- 06.wc/wc_cmd.rb
+++ 06.wc/wc_cmd.rb
@@ -7,47 +7,41 @@ def main
   files = ARGV
   opts = ARGV.getopts('l')
   if files.empty?
-    text = readlines
-    display_format(text, opts)
+    file = readlines
+    show_file_info(file.count, file.to_s.split.size, file.sum(&:bytesize), opts)
     puts "\n"
   else
-    with_files(files, opts)
+    process_files(files, opts)
   end
 end
 
-# ファイル指定あり
-def with_files(files, opts)
-  line_count = []
-  word_count = []
-  bytesize = []
+# ファイル指定が有る場合の処理
+def process_files(files, opts)
+  line_count_lists = []
+  word_count_lists = []
+  bytesize_lists = []
   files.each do |file|
-    text = File.read(file).lines
-    line_count << text.size
-    word_count << text.to_s.split.size
-    bytesize << text.sum(&:bytesize)
-    display_format(text, opts)
+    readed_file = File.read(file)
+    line_count = readed_file.count("\n")
+    word_count = readed_file.scan(/\s+/).count
+    line_count_lists << line_count
+    word_count_lists << word_count
+    bytesize_lists << readed_file.bytesize
+    show_file_info(line_count, word_count, readed_file.bytesize, opts)
     puts " #{file}"
   end
-  return if files.size == 1
+  return if files.count == 1
 
-  if opts['l']
-    show_info(line_count.sum)
-  else
-    show_info(line_count.sum, word_count.sum, bytesize.sum)
-  end
+  show_file_info(line_count_lists.sum, word_count_lists.sum, bytesize_lists.sum, opts)
   puts ' total'
 end
 
-def display_format(text, opts)
+def show_file_info(line_count, word_count, bytesize, opts)
   if opts['l']
-    show_info(text.size)
+    print line_count.to_s.rjust(8)
   else
-    show_info(text.size, text.to_s.split.size, text.sum(&:bytesize))
+    [line_count, word_count, bytesize].each { |info| print info.to_s.rjust(8) }
   end
-end
-
-def show_info(*file_info_lists)
-  file_info_lists.each { |list| print list.to_s.rjust(8) }
 end
 
 main
