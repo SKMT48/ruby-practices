371e166ed506fa218e1870b139a38b3c2add6e4d
Sakamoto <tongararin@gmail.com>
2021-03-21 11:22:06 +0900

    processメソッドのロジックを変更し行数を削減、テキストの行数・単語数・バイトサイズをハッシュから受け取るように変更しました

--- 06.wc/wc_cmd.rb
+++ 06.wc/wc_cmd.rb
@@ -7,8 +7,8 @@ def main
   files = ARGV
   opts = ARGV.getopts('l')
   if files.empty?
-    file = readlines
-    show_file_info(file.count, file.to_s.split.size, file.sum(&:bytesize), opts)
+    text_array = readlines
+    show_file_info(text_array.join, opts)
     puts "\n"
   else
     process_files(files, opts)
@@ -17,30 +17,25 @@ end
 
 # ファイル指定が有る場合の処理
 def process_files(files, opts)
-  line_count_lists = []
-  word_count_lists = []
-  bytesize_lists = []
+  file_text_list = []
   files.each do |file|
-    readed_file = File.read(file)
-    line_count = readed_file.count("\n")
-    word_count = readed_file.scan(/\s+/).count
-    line_count_lists << line_count
-    word_count_lists << word_count
-    bytesize_lists << readed_file.bytesize
-    show_file_info(line_count, word_count, readed_file.bytesize, opts)
+    file_text = File.read(file)
+    show_file_info(file_text, opts)
     puts " #{file}"
+    file_text_list << file_text
   end
   return if files.count == 1
 
-  show_file_info(line_count_lists.sum, word_count_lists.sum, bytesize_lists.sum, opts)
+  show_file_info(file_text_list.join, opts)
   puts ' total'
 end
 
-def show_file_info(line_count, word_count, bytesize, opts)
+def show_file_info(text, opts)
+  text_info_list = { line_count: text.count("\n"), word_count: text.scan(/\s+/).count, bytesize: text.bytesize }
   if opts['l']
-    print line_count.to_s.rjust(8)
+    print text_info_list[:line_count].to_s.rjust(8)
   else
-    [line_count, word_count, bytesize].each { |info| print info.to_s.rjust(8) }
+    text_info_list.each_value { |info| print info.to_s.rjust(8) }
   end
 end
 
